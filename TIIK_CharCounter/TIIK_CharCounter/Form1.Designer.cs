e6b8464e94a358123639ff1827e58294a3792674
MalgorzataMarczykzyk <malgorzata_marczyk@wp.pl>
2019-10-18 18:06:31 +0200

    Count char in readed file txt

---
+++ TIIK_CharCounter/TIIK_CharCounter/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace TIIK_CharCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonChooseFile = new System.Windows.Forms.Button();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.labelFilePath = new System.Windows.Forms.Label();
+            this.richTextBoxCountData = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonCount = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonChooseFile
+            // 
+            this.buttonChooseFile.Location = new System.Drawing.Point(180, 51);
+            this.buttonChooseFile.Name = "buttonChooseFile";
+            this.buttonChooseFile.Size = new System.Drawing.Size(92, 27);
+            this.buttonChooseFile.TabIndex = 0;
+            this.buttonChooseFile.Text = "Choose file";
+            this.buttonChooseFile.UseVisualStyleBackColor = true;
+            this.buttonChooseFile.Click += new System.EventHandler(this.buttonChooseFile_Click);
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(71, 25);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(309, 20);
+            this.textBoxFilePath.TabIndex = 1;
+            // 
+            // labelFilePath
+            // 
+            this.labelFilePath.AutoSize = true;
+            this.labelFilePath.Location = new System.Drawing.Point(208, 9);
+            this.labelFilePath.Name = "labelFilePath";
+            this.labelFilePath.Size = new System.Drawing.Size(48, 13);
+            this.labelFilePath.TabIndex = 2;
+            this.labelFilePath.Text = "File Path";
+            // 
+            // richTextBoxCountData
+            // 
+            this.richTextBoxCountData.Location = new System.Drawing.Point(15, 175);
+            this.richTextBoxCountData.Name = "richTextBoxCountData";
+            this.richTextBoxCountData.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBoxCountData.Size = new System.Drawing.Size(446, 165);
+            this.richTextBoxCountData.TabIndex = 3;
+            this.richTextBoxCountData.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Char";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(89, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 47);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Number of occurrences in the text";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Probability";
+            // 
+            // buttonCount
+            // 
+            this.buttonCount.Location = new System.Drawing.Point(180, 96);
+            this.buttonCount.Name = "buttonCount";
+            this.buttonCount.Size = new System.Drawing.Size(92, 23);
+            this.buttonCount.TabIndex = 7;
+            this.buttonCount.Text = "Count";
+            this.buttonCount.UseVisualStyleBackColor = true;
+            this.buttonCount.Click += new System.EventHandler(this.buttonCount_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(367, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 47);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Number of information in the text";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 352);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBoxCountData);
+            this.Controls.Add(this.labelFilePath);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.buttonChooseFile);
+            this.Name = "Form1";
+            this.Text = "TIIK Char Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonChooseFile;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Label labelFilePath;
+        private System.Windows.Forms.RichTextBox richTextBoxCountData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonCount;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TIIK_CharCounter/TIIK_CharCounter/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TIIK_CharCounter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        public StreamReader sReader;
+        public FileStream fStream;
+        private void buttonChooseFile_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Text|*.txt|All|*.*";
+           if( ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = ofd.FileName;
+                sReader = new StreamReader(File.OpenRead(ofd.FileName));
+            }
+
+        }
+
+        void countTextData(string sRader)
+        {
+            var map = new Dictionary<char, double>();
+            foreach(char c in sRader)
+            {
+                if (map.ContainsKey(c))
+                {
+                    map[c]++;
+                }
+                else
+                {
+                    int letterNumer = (int)c;
+                    if((letterNumer >= 65 && letterNumer <= 90) || //duze litery
+                       (letterNumer >= 97 && letterNumer <= 122) || //małe litery
+                       (letterNumer >= 192 && letterNumer <= 687))  //znaki specjalne
+                    {
+                        map.Add(c, 1);
+                    }
+                    
+                }
+            }
+            foreach (KeyValuePair<char, double> entry in map)
+            {
+                richTextBoxCountData.AppendText(entry.Key +"\t\t" + entry.Value.ToString() + "\n");
+            }
+            
+        }
+
+        private void buttonCount_Click(object sender, EventArgs e)
+        { 
+            string readFileText = sReader.ReadToEnd();
+            countTextData(readFileText);
+
+           
+        }
+    }
+}
